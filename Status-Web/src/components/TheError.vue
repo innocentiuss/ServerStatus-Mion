31b979e17a9a287a2118a4bda61211bfc86df1e8
szncu <549528220@qq.com>
2022-06-29 02:37:52 +0800

    feature: beautify loading

--- Status-Web/src/components/TheError.vue
+++ Status-Web/src/components/TheError.vue
@@ -1,7 +1,11 @@
 <template>
-  <p class="error">
-    若長時間出現此消息，請確保您已啟用Javascript！<br>否則雲監控主服務（服務端）未啟動或已關閉。
-  </p>
+  <div class="ui raised very padded text container segment">
+    <div class="ui active centered inline loader"></div>
+    <h2 class="ui header"></h2>
+    <p class="error">
+      正在连接中, 若长时间出现此消息，请确保您已启用Javascript！<br>否则云监控主服务（Java服务端）未启动或已关闭。
+    </p>
+  </div>
 </template>
 <script lang="ts">
 import { defineComponent } from 'vue';
